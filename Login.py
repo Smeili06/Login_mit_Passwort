ea6834ddc042e5d9fa3a08e54ff8decf23338645
Smeili06 <90852785+Smeili06@users.noreply.github.com>
2021-12-14 16:10:01 +0100

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,15 @@
+Versuche = 3
+while Versuche > 0:
+    username = input(str("Gib den Benutzernamen ein: "))
+    password = input(str("Gib das Passwort ein: "))
+    if username == str("Maël1"):
+        if password == str("Test35"):
+            print("Herzlich Willkommen")
+            break
+        else:
+            print("Das Passwort ist nicht korrekt")
+            Versuche = Versuche -1
+    else:
+        print("Der Benutzername ist falsch")
+    if Versuche == 0:
+        print("Zu viele Loginversuche. Aus Sicherheitsgründen wird das Konto nun gesperrt.")    
